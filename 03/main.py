5653aeb53d2dddebdd548b22419c7b8183e33abb
Csaba Andras <acsaba@gmail.com>
2025-01-23 01:59:06 +0100

    day 03 after watching guide

--- 03/main.py
+++ 03/main.py
@@ -1,36 +1,63 @@
+from dataclasses import dataclass
+from typing import Optional
+import pprint
+import string
 
-def xorRepeat(buf: bytearray, c: int) -> bytearray:
+def xorRepeat(buf: bytes, c: int) -> bytes:
     ret = bytearray(len(buf))
     for i, v in enumerate(buf):
         ret[i] = v ^ c
-    return ret
+    return bytes(ret)
 
-def countLetters(buf: bytearray) -> int:
+def countLetters(buf: bytes) -> int:
     ret = 0
     for v in buf:
         if ord('A') <= v and v <= ord('Z'):
             ret += 1
         elif ord('a') <= v and v <= ord('z'):
             ret += 1
+        elif v == ord(' '):
+            ret += 1
     return ret
 
+@dataclass(order=True)
+class ScoredGuess:
+    score: float = float('-inf')
+    key: Optional[int] = None
+    chiperText: Optional[bytes] = None
+    plainText: Optional[bytes] = None
 
-def findKey(chiper: bytearray) -> tuple[int, bytearray]:
-    bestScore = 0
-    bestText = bytearray()
-    bestKey = 0
+    @classmethod
+    def FromKey(cls, chiperText: bytes, key_: int):
+        plainText = xorRepeat(chiperText, key_)
+        score = countLetters(plainText)
+        return cls(score, key_, chiperText, plainText)
+
+def crackXorCipherSimple(chiper: bytes) -> ScoredGuess:
+    bestGuess = ScoredGuess()
     for i in range(256):
-        text = xorRepeat(chiper, i)
-        score = countLetters(text)
-        # print(f'{i} -> {score=}')
-        if bestScore < score:
-            bestScore = score
-            bestText = text
-            bestKey = i
+        bestGuess = max(bestGuess, ScoredGuess.FromKey(chiper, i))
 
-    return bestKey, bestText
+    return bestGuess
+
+def crackXorCipher(chiper: bytes) -> ScoredGuess:
+    bestGuess = ScoredGuess()
+    counts = {v: chiper.count(v) for v in chiper}
+
+    goodChars = string.ascii_letters + ' '
+    for i in range(256):
+        score = 0
+        for c in goodChars:
+            score += counts.get(ord(c)^i, 0)
+        bestGuess = max(bestGuess, ScoredGuess(score, i))
+
+    bestGuess.chiperText = chiper
+    bestGuess.chiperText = xorRepeat(chiper, bestGuess.key)
+    return bestGuess
+
 
 if __name__ == "__main__":
-    chiper = bytearray.fromhex('1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736')
+    chiper = bytes.fromhex('1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736')
     print(f'{chiper=}')
-    print(findKey(chiper))
+    pprint.pprint(crackXorCipherSimple(chiper))
+    pprint.pprint(crackXorCipher(chiper))
